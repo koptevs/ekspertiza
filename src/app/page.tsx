79603bdcae2cad133243ae4aa0ae77a60baf46d9
Igors Koptevs <igors.koptevs@gmail.com>
2026-03-04 19:06:15 +0200

    Files moved

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,4 @@
-import { ComponentExample } from "@/components/component-example";
+import { ComponentExample } from "@/components/shared/component-example";
 
 export default function Page() {
 return <ComponentExample />;
